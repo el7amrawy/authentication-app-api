da3ef3f529a773e844cb16b34f58b1f307e14dbd
Aly <el7amrawy@outlook.com>
2022-11-03 20:16:52 +0200

    add index, show, create to users model

---
+++ src/models/users.ts
@@ -0,0 +1,66 @@
+import client from "../database";
+
+type User = {
+  id: string;
+  username: string;
+  name: string;
+  phone: string;
+  email: string;
+  bio: string;
+  password?: string;
+};
+
+class Users {
+  async index(): Promise<User[]> {
+    const sql =
+      "SELECT (id , username , name , phone , email , bio) FROM users";
+
+    try {
+      const conn = await client.connect();
+      const res = await conn.query(sql);
+      const users: User[] = res.rows;
+
+      conn.release();
+
+      return users;
+    } catch (err) {
+      throw new Error(`db error couldn't show users ==> ${err}`);
+    }
+  }
+
+  async show(username: string): Promise<User> {
+    const sql =
+      "SELECT (id , username , name , phone , email , bio) WHERE username=($1)";
+
+    try {
+      const conn = await client.connect();
+      const res = await conn.query(sql, [username]);
+      const user: User = res.rows[0];
+
+      conn.release();
+
+      return user;
+    } catch (err) {
+      throw new Error(`db error couldn't show user ${username} ==> ${err}`);
+    }
+  }
+
+  async create(email: string, password: string): Promise<User> {
+    const sql =
+      "INSERT INTO users (email,password) VALUES ($1,$2) RETURNING (id , username , name , phone , email , bio)";
+
+    try {
+      const conn = await client.connect();
+      const res = await conn.query(sql, [email, password]);
+      const user: User = res.rows[0];
+
+      conn.release();
+
+      return user;
+    } catch (err) {
+      throw new Error(`db error couldn't create user ==> ${err}`);
+    }
+  }
+}
+
+export { User, Users };
