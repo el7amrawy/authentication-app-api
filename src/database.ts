9679838780f60166403776e555f49ee43951b489
Aly <el7amrawy@outlook.com>
2022-11-30 23:51:56 +0200

    pipeline

--- src/database.ts
+++ src/database.ts
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const {
-  POSTGRES_HOST,
+  // POSTGRES_HOST,
   POSTGRES_USER,
   POSTGRES_PASSWORD,
   POSTGRES_PORT,
@@ -16,7 +16,7 @@ let { ENV } = process.env;
 ENV = ENV?.replace(/\s/g, "");
 
 const client = new pg.Pool({
-  host: POSTGRES_HOST,
+  // host: POSTGRES_HOST,
   port: parseInt(POSTGRES_PORT as unknown as string),
   database:
     ENV === "dev" ? POSTGRES_DB : ENV === "test" ? POSTGRES_TEST_DB : "",
