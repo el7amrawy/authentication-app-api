34b71e7abc273bad6db44985c5852bc40341ce82
Aly <el7amrawy@outlook.com>
2022-12-01 13:09:56 +0200

    add pipline

--- src/database.ts
+++ src/database.ts
@@ -4,24 +4,46 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const {
-  POSTGRES_HOST,
   POSTGRES_USER,
   POSTGRES_PASSWORD,
-  POSTGRES_PORT,
   POSTGRES_DB,
+  // local test
   POSTGRES_TEST_DB,
+  // circleci
+  CI_POSTGRES_USER,
+  CI_POSTGRES_PASSWORD,
+  CI_POSTGRES_DB,
 } = process.env;
 
 let { ENV } = process.env;
 ENV = ENV?.replace(/\s/g, "");
 
-const client = new pg.Pool({
-  host: POSTGRES_HOST,
-  port: parseInt(POSTGRES_PORT as unknown as string),
-  database:
-    ENV === "dev" ? POSTGRES_DB : ENV === "test" ? POSTGRES_TEST_DB : "",
+const dev = {
+  database: POSTGRES_DB,
   user: POSTGRES_USER,
   password: POSTGRES_PASSWORD,
-});
+};
+
+const test = {
+  database: POSTGRES_TEST_DB,
+  user: POSTGRES_USER,
+  password: POSTGRES_PASSWORD,
+};
+
+const circleci = {
+  database: CI_POSTGRES_DB,
+  user: CI_POSTGRES_USER,
+  password: CI_POSTGRES_PASSWORD,
+};
+
+const client = new pg.Pool(
+  ENV === "dev"
+    ? dev
+    : ENV === "test"
+    ? test
+    : ENV === "circleci"
+    ? circleci
+    : {}
+);
 
 export default client;
