98571f0ba754c40feb2c08a57df47b7c55841aee
Aly <el7amrawy@outlook.com>
2022-12-02 14:43:20 +0200

    add PORT

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   process.stdout.write(`server started at http://localhost:${port}\n`);
